548d1f6e069976ff724d93ca90caa6c45925423c
David Vinicius Da Silva <de.175@hotmail.com>
2019-08-22 20:41:41 -0300

    Implementado a escolha das categorias

--- art_fuzzy.py
+++ art_fuzzy.py
@@ -2,15 +2,44 @@ import numpy as np
 from funcoes import *
 
 v = np.array([
-    [7, 4.5],
-    [2, 3]
+    [7, 4.5, 6],
+    [2, 3, 9]
 ])
+
+Y     = []
+
+alpha = 0.0001
+p     = 0.5
+beta  = 1
 
 valueMax = 10
 
 IC = normalize(v, valueMax)
 IC = complement(IC)
 
+W  = np.ones(IC.shape)
+T  = []
 
+for i in range(0, len(IC)):
+    a = np.sum(AND(IC[i], W[i]))
+    T.append(a / (alpha + np.sum(W[i])))
 
+champion      = np.amax(T)
+championIndex = np.argmin(T)
+
+teste = np.sum(AND(IC[championIndex], W[championIndex])) / np.sum(IC[championIndex])
+
+if teste > p:
+    print("OCORREU RECORRÊNCIA", teste)
+    Y.append(np.zeros(len(IC)))
+    #Y[championIndex] = 1
+
+print("I choose my champion:", champion ,championIndex)
+
+print(Y)
+'''
 print(IC)
+print("-----")
+print("-----")
+print(W)
+'''
--- funcoes.py
+++ funcoes.py
@@ -12,8 +12,16 @@ def complement(I):
     IC = np.subtract(1, I)
     A  = []
 
-    for i in range(0, 2):
+    for i in range(0, len(I)):
         #print(np.concatenate((I[i], IC[i])))
+        #I[i].extend(list(IC))
         A.append( np.concatenate((I[i], IC[i])) )
     
     return np.array(A)
+
+def AND(arr1, arr2):
+    I = []
+    for i in range(0, len(arr1)):
+        I.append(min(arr1[i], arr2[i]))
+    return I
+

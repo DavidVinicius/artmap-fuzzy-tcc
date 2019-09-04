6e84bb22b6aa799198d5ae0eb8d02dab7e13e696
David Vinicius Da Silva <de.175@hotmail.com.br>
2019-09-04 20:42:50 -0300

    Ajuste na classe

--- art.py
+++ art.py
@@ -1,5 +1,7 @@
 class ART:
-    def __init__(I, alpha = 0.001, rho = 0.5, beta = 1):
+    Y = []
+
+    def __init__(self, I, alpha = 0.001, rho = 0.5, beta = 1):
         self.I     = I
         self.alpha = alpha
         self.rho   = rho
